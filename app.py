d42495d3c2e17e721d9cf07ef95fad8905e8b443
JLBKMVE <jlbkmve@gmail.com>
2024-11-20 11:02:46 +0100

    Adjusting first page

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ def next_step():
     st.session_state['step'] += 1
 
 def prev_step():
-    st.session_state['step'] -= 1
+    if st.session_state['step'] > 1:
+        st.session_state['step'] -= 1
 
 def reset_steps():
     st.session_state['step'] = 1
@@ -23,113 +24,75 @@ st.set_page_config(page_title="IPM - Innovation Portfolio Management")
 
 # Step 1
 if st.session_state['step'] == 1:
-    st.title('Please select your innovation portfolio characteristics and criteria')
+    st.title('What is the focus of your innovation portfolio?')
 
-    # Geofocus
-    geofocus_options = [
-        'Balanced across initiatives countries',
-        'Balance across CGIAR regions',
-        'Focus on selected countries'
-    ]
-    geofocus = st.selectbox('Geofocus:', geofocus_options)
+    st.write('Please select the focus areas of your innovation portfolio. You can select as many as you want.')
 
-    if geofocus == 'Focus on selected countries':
-        countries_list = ['Afghanistan', 'Brazil', 'Canada', 'Denmark', 'Ethiopia']  # Replace with actual country names
-        selected_countries = st.multiselect('Select countries:', countries_list)
-    else:
-        selected_countries = None
+    focus_selected = False  # To ensure at least one focus area is selected
 
-    # Innovation Readiness
-    innovation_readiness_options = [
-        'Balanced across Innovation readiness levels',
-        'Tailored innovation readiness focus'
-    ]
-    innovation_readiness = st.selectbox('Innovation Readiness:', innovation_readiness_options)
+    entire_cgiar = st.checkbox('Entire CGIAR', key='entire_cgiar')
+    if entire_cgiar:
+        focus_selected = True
+        st.session_state['data']['entire_cgiar'] = True
 
-    if innovation_readiness == 'Tailored innovation readiness focus':
-        st.write('Please select the focus areas and specify the percentage for each (total must sum to 100%):')
-        focus_areas = {}
-        total_percentage = 0
-        cols = st.columns(2)
-        with cols[0]:
-            if st.checkbox('Ideation and research'):
-                percentage = st.number_input('Percentage for Ideation and research (%)', min_value=0, max_value=100, value=0, key='Ideation')
-                focus_areas['Ideation and research'] = percentage
-                total_percentage += percentage
-            if st.checkbox('Incubation'):
-                percentage = st.number_input('Percentage for Incubation (%)', min_value=0, max_value=100, value=0, key='Incubation')
-                focus_areas['Incubation'] = percentage
-                total_percentage += percentage
-        with cols[1]:
-            if st.checkbox('Acceleration'):
-                percentage = st.number_input('Percentage for Acceleration (%)', min_value=0, max_value=100, value=0, key='Acceleration')
-                focus_areas['Acceleration'] = percentage
-                total_percentage += percentage
-            if st.checkbox('Scaling-ready'):
-                percentage = st.number_input('Percentage for Scaling-ready (%)', min_value=0, max_value=100, value=0, key='Scaling-ready')
-                focus_areas['Scaling-ready'] = percentage
-                total_percentage += percentage
-        if total_percentage != 100:
-            st.warning('Total percentage must sum to 100%')
-    else:
-        focus_areas = None
+    initiative_program = st.checkbox('Initiative/ Program', key='initiative_program')
+    if initiative_program:
+        focus_selected = True
+        initiatives_list = ['Initiative A', 'Initiative B', 'Initiative C']  # Replace with actual initiatives
+        selected_initiatives = st.multiselect('If yes, which Initiative(s)/ Program(s):', initiatives_list, key='selected_initiatives')
+        st.session_state['data']['selected_initiatives'] = selected_initiatives
 
-    # End of Initiative Outcomes (EoIO)
-    eoio_options = [
-        'Balanced across End of Initiative Outcomes (EoIO)',
-        'Tailored End of Initiative Outcomes (EoIO)'
-    ]
-    eoio = st.selectbox('End of Initiative Outcomes (EoIO):', eoio_options)
+    cgiar_centre = st.checkbox('CGIAR-centre', key='cgiar_centre')
+    if cgiar_centre:
+        focus_selected = True
+        centres_list = ['Centre 1', 'Centre 2', 'Centre 3']  # Replace with actual CGIAR-centre(s)
+        selected_centres = st.multiselect('If yes, which CGIAR-centre(s):', centres_list, key='selected_centres')
+        st.session_state['data']['selected_centres'] = selected_centres
 
-    if eoio == 'Tailored End of Initiative Outcomes (EoIO)':
-        st.write('Please select the EoIOs and specify the percentage for each (total must sum to 100%):')
-        eoio_areas = {}
-        total_eoio_percentage = 0
-        cols = st.columns(2)
-        with cols[0]:
-            if st.checkbox('EoIO1'):
-                percentage = st.number_input('Percentage for EoIO1 (%)', min_value=0, max_value=100, value=0, key='EoIO1')
-                eoio_areas['EoIO1'] = percentage
-                total_eoio_percentage += percentage
-            if st.checkbox('EoIO2'):
-                percentage = st.number_input('Percentage for EoIO2 (%)', min_value=0, max_value=100, value=0, key='EoIO2')
-                eoio_areas['EoIO2'] = percentage
-                total_eoio_percentage += percentage
-        with cols[1]:
-            if st.checkbox('EoIO3'):
-                percentage = st.number_input('Percentage for EoIO3 (%)', min_value=0, max_value=100, value=0, key='EoIO3')
-                eoio_areas['EoIO3'] = percentage
-                total_eoio_percentage += percentage
-            if st.checkbox('EoIO4'):
-                percentage = st.number_input('Percentage for EoIO4 (%)', min_value=0, max_value=100, value=0, key='EoIO4')
-                eoio_areas['EoIO4'] = percentage
-                total_eoio_percentage += percentage
-        if total_eoio_percentage != 100:
-            st.warning('Total percentage must sum to 100%')
-    else:
-        eoio_areas = None
+    country_region = st.checkbox('Country or Region', key='country_region')
+    if country_region:
+        focus_selected = True
+        countries_list = ['Afghanistan', 'Brazil', 'Canada', 'Denmark', 'Ethiopia']  # Replace with actual countries or regions
+        selected_countries = st.multiselect('If yes, which country/ies or region(s):', countries_list, key='selected_countries')
+        st.session_state['data']['selected_countries'] = selected_countries
 
-    if st.button('Next'):
-        st.session_state['data']['geofocus'] = geofocus
-        if selected_countries:
-            st.session_state['data']['selected_countries'] = selected_countries
-        st.session_state['data']['innovation_readiness'] = innovation_readiness
-        if focus_areas:
-            st.session_state['data']['focus_areas'] = focus_areas
-        st.session_state['data']['eoio'] = eoio
-        if eoio_areas:
-            st.session_state['data']['eoio_areas'] = eoio_areas
+    thematic_area = st.checkbox('Thematic Area', key='thematic_area')
+    if thematic_area:
+        focus_selected = True
+        thematic_areas_list = ['Agriculture', 'Climate Change', 'Food Security']  # Replace with actual thematic areas
+        selected_thematic_areas = st.multiselect('If yes, which thematic areas:', thematic_areas_list, key='selected_thematic_areas')
+        st.session_state['data']['selected_thematic_areas'] = selected_thematic_areas
 
-        valid = True
-        if innovation_readiness == 'Tailored innovation readiness focus' and total_percentage != 100:
-            valid = False
-            st.warning('Please ensure total percentages sum to 100% for Innovation Readiness.')
-        if eoio == 'Tailored End of Initiative Outcomes (EoIO)' and total_eoio_percentage != 100:
-            valid = False
-            st.warning('Please ensure total percentages sum to 100% for EoIO.')
+    funder = st.checkbox('Funder', key='funder')
+    if funder:
+        focus_selected = True
+        funders_list = ['Funder A', 'Funder B', 'Funder C']  # Replace with actual funders
+        selected_funders = st.multiselect('If yes, which funders:', funders_list, key='selected_funders')
+        st.session_state['data']['selected_funders'] = selected_funders
 
-        if valid:
-            next_step()
+    fund_type = st.checkbox('Fund-type', key='fund_type')
+    if fund_type:
+        focus_selected = True
+        fund_type_options = ['Pooled', 'Non-pooled']
+        selected_fund_type = st.selectbox('Pooled/ non-pooled:', fund_type_options, key='selected_fund_type')
+        st.session_state['data']['selected_fund_type'] = selected_fund_type
+
+    # Additional focus
+    st.write('Is there a different focus you would like to apply, that is not reflected above?')
+    additional_focus = st.text_area('Please specify:', key='additional_focus')
+    if additional_focus:
+        st.session_state['data']['additional_focus'] = additional_focus
+
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button('Back', key='back1'):
+            prev_step()
+    with col2:
+        if st.button('Next', key='next1'):
+            if focus_selected or additional_focus:
+                next_step()
+            else:
+                st.warning('Please select at least one focus area or provide an additional focus.')
 
 # Step 2
 elif st.session_state['step'] == 2:
@@ -147,21 +110,23 @@ elif st.session_state['step'] == 2:
 
     selected_options = []
     for option in options:
-        if st.checkbox(option):
+        if st.checkbox(option, key=f"option_{option}"):
             selected_options.append(option)
 
     additional_areas = st.text_input('Are there any areas that you would like to prioritize, but are not reflected in the above options?')
 
-    if st.button('Next'):
-        if selected_options:
-            st.session_state['data']['prioritized_areas'] = selected_options
-            st.session_state['data']['additional_prioritized_areas'] = additional_areas
-            next_step()
-        else:
-            st.warning('Please select at least one area to prioritize.')
-
-    if st.button('Back'):
-        prev_step()
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button('Back', key='back2'):
+            prev_step()
+    with col2:
+        if st.button('Next', key='next2'):
+            if selected_options or additional_areas:
+                st.session_state['data']['prioritized_areas'] = selected_options
+                st.session_state['data']['additional_prioritized_areas'] = additional_areas
+                next_step()
+            else:
+                st.warning('Please select at least one area to prioritize or specify additional areas.')
 
 # Step 3
 elif st.session_state['step'] == 3:
@@ -192,7 +157,7 @@ elif st.session_state['step'] == 3:
             selected_clients = st.multiselect('Select target clients:', target_clients, key='target_clients')
             st.session_state['data']['target_clients_detail'] = selected_clients
         elif 'Sustainable Development Goals' in option:
-            sdg_options = [f'SDG {i}' for i in range(1,18)]
+            sdg_options = [f'SDG {i}' for i in range(1, 18)]
             selected_sdgs = st.multiselect('Select SDGs:', sdg_options, key='sdgs')
             st.session_state['data']['sdgs_detail'] = selected_sdgs
         elif 'Megatrends' in option:
@@ -204,47 +169,52 @@ elif st.session_state['step'] == 3:
             selected_commodities = st.multiselect('Select commodities:', commodities, key='commodities')
             st.session_state['data']['commodities_detail'] = selected_commodities
 
-    if st.button('Next'):
-        next_step()
-    if st.button('Back'):
-        prev_step()
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button('Back', key='back3'):
+            prev_step()
+    with col2:
+        if st.button('Next', key='next3'):
+            next_step()
 
 # Step 4
 elif st.session_state['step'] == 4:
     st.title('Please select your innovation portfolio restrictions')
 
-    # Polled USD investment available
-    st.subheader('Polled USD investment available')
-    min_value = st.number_input('Minimum investment amount ($):', min_value=0, max_value=5000000, value=0)
-    max_value = st.number_input('Maximum investment amount ($):', min_value=0, max_value=5000000, value=5000000)
+    # Pooled USD investment available
+    st.subheader('Pooled USD investment available')
+    min_value = st.number_input('Minimum investment amount ($):', min_value=0, max_value=5000000, value=0, key='min_investment')
+    max_value = st.number_input('Maximum investment amount ($):', min_value=0, max_value=5000000, value=5000000, key='max_investment')
     st.session_state['data']['investment_min'] = min_value
     st.session_state['data']['investment_max'] = max_value
 
     # Priority Partnerships
     st.subheader('Priority Partnerships')
     partnership_options = ['Private sector', 'Government', 'NGO', 'Universities', 'Other']
-    priority_partnership = st.selectbox('Select priority partnership:', partnership_options)
+    priority_partnership = st.selectbox('Select priority partnership:', partnership_options, key='priority_partnership')
     if priority_partnership == 'Other':
-        other_partnership = st.text_input('Please specify other partnerships:')
+        other_partnership = st.text_input('Please specify other partnerships:', key='other_partnership')
         st.session_state['data']['priority_partnership'] = other_partnership
     else:
         st.session_state['data']['priority_partnership'] = priority_partnership
 
     # Risk profile
     st.subheader('Risk profile')
-    risk_profile = st.radio('Select risk profile:', ['Low', 'Medium', 'High'])
+    risk_profile = st.radio('Select risk profile:', ['Low', 'Medium', 'High'], key='risk_profile')
     st.session_state['data']['risk_profile'] = risk_profile
 
-    if st.button('Submit'):
-        # Output JSON and store data in pandas DataFrame
-        data_df = pd.DataFrame([st.session_state['data']])
-        st.session_state['data_df'] = data_df
-        st.success('Your data has been collected successfully.')
-        st.json(st.session_state['data'])
-        next_step()
-
-    if st.button('Back'):
-        prev_step()
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button('Back', key='back4'):
+            prev_step()
+    with col2:
+        if st.button('Submit', key='submit'):
+            # Output JSON and store data in pandas DataFrame
+            data_df = pd.DataFrame([st.session_state['data']])
+            st.session_state['data_df'] = data_df
+            st.success('Your data has been collected successfully.')
+            st.json(st.session_state['data'])
+            next_step()
 
 # Step 5
 elif st.session_state['step'] == 5:
@@ -258,12 +228,14 @@ elif st.session_state['step'] == 5:
 
     # Display Action Areas (placeholder)
     st.subheader('Action Areas')
-    action_areas = ['Nutrition, Health & Food Security',
-                    'Poverty Reduction, Livelihoods & Jobs',
-                    'Gender Equality, Youth & Social Inclusion',
-                    'Climate Adaptation & Mitigation',
-                    'Environmental Health & Biodiversity']
-    action_percentages = [random.randint(10,30) for _ in action_areas]
+    action_areas = [
+        'Nutrition, Health & Food Security',
+        'Poverty Reduction, Livelihoods & Jobs',
+        'Gender Equality, Youth & Social Inclusion',
+        'Climate Adaptation & Mitigation',
+        'Environmental Health & Biodiversity'
+    ]
+    action_percentages = [random.randint(10, 30) for _ in action_areas]
     action_data = pd.DataFrame({
         'Action Area': action_areas,
         'Percentage': action_percentages
@@ -274,21 +246,23 @@ elif st.session_state['step'] == 5:
     st.subheader('Sustainable Development Goals (SDGs)')
     sdg_data = pd.DataFrame({
         'SDG': [f'SDG {i}' for i in range(1, 8)],
-        'Value': [random.randint(1,10) for _ in range(1,8)]
+        'Value': [random.randint(1, 10) for _ in range(1, 8)]
     })
     st.line_chart(sdg_data.set_index('SDG'))
 
-    # Display collected data (placeholder)
+    # Display collected data
     st.subheader('Your Input Summary')
     st.dataframe(st.session_state['data_df'])
 
-    if st.button('Refresh information'):
-        reset_steps()
-
-    if st.button('Back'):
-        prev_step()
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button('Back', key='back5'):
+            prev_step()
+    with col2:
+        if st.button('Restart', key='restart'):
+            reset_steps()
 
 # Footer or Navigation (Optional)
 st.sidebar.title('Navigation')
-if st.sidebar.button('Restart'):
+if st.sidebar.button('Restart', key='restart_sidebar'):
     reset_steps()
